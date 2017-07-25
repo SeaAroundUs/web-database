d1889176420653c5aed8b55dacbdf7164a5725f7
gtsui <Tsui.gordon.9@gmail.com>
2017-07-25 15:38:53 -0700

    Update dump_db.bat
    Added web_partition schema in order for cell_catch table to be populated

--- data_dump/dump_db.bat
+++ data_dump/dump_db.bat
@@ -18,7 +18,7 @@ mkdir db_dump
 IF EXIST db_dump\toc.dat DEL db_dump\*
 
 echo Password for user sau
-pg_dump -h %DbServer% -p %DbPort% -f db_dump -T web.django_migrations -T admin.django_migrations -Fd -E UTF8 -j 8 -O --no-unlogged-table-data -U sau -n web -n web_cache -n fao -n geo -n feru -n expedition -n distribution -n admin sau
+pg_dump -h %DbServer% -p %DbPort% -f db_dump -T web.django_migrations -T admin.django_migrations -Fd -E UTF8 -j 8 -O --no-unlogged-table-data -U sau -n web -n web_cache -n fao -n geo -n feru -n expedition -n distribution -n admin -n web_partition sau
 
 @ECHO OFF
 
