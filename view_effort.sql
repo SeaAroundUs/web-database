3a051de3d6a93d558e7eba03eedeae81440b2186
gordontsui <g.tsui@oceans.ubc.ca>
2019-06-18 16:02:35 -0700

    Add gear id to view

--- view_effort.sql
+++ view_effort.sql
@@ -1,9 +1,10 @@
 CREATE OR REPLACE VIEW fishing_effort.v_fishing_effort
-AS WITH base_table(fishing_entity_id, year, sector_type_id, eez_id, effort_gear_id, length_code, m_um, motorisation, kw_boat, number_boats, days_fished, em_factor, sfr, fuel_coeff) AS (
+AS WITH base_table(fishing_entity_id, year, sector_type_id, eez_id, gear_id ,effort_gear_id, length_code, m_um, motorisation, kw_boat, number_boats, days_fished, em_factor, sfr, fuel_coeff) AS (
          SELECT fe.fishing_entity_id,
             fe.year,
             fe.sector_type_id,
             fe.eez_id,
+			fg.gear_id,
             fe.effort_gear_id,
             fe.length_code,
             fe.m_um,
@@ -52,11 +53,14 @@ AS WITH base_table(fishing_entity_id, year, sector_type_id, eez_id, effort_gear_
             fc.fuel_coeff
            FROM fishing_effort.fishing_effort fe
              JOIN fishing_effort.fuel_coeff fc ON fe.year = fc."﻿year"
+             join web.sector_type st on st.sector_type_id = fe.sector_type_id
+			 JOIN fishing_effort.fishing_effort_gear fg ON st.name = fg.sector_type and fe.effort_gear_id = fg.effort_gear_id
         )
  SELECT base_table.fishing_entity_id,
     base_table.year,
     base_table.sector_type_id,
     base_table.eez_id,
+	base_table.gear_id,
     base_table.effort_gear_id,
     base_table.length_code,
     base_table.m_um,
