2e9ae863dca541798cbf271f0a76181a395e4d11
Miel <s.ortiz@q-quatics.org>
2020-08-07 23:50:14 +0800

    Update v_fishing_effort view
    Updated v_fishing_effort view from QA

--- view_effort.sql
+++ view_effort.sql
@@ -1,32 +1,32 @@
 ﻿CREATE OR REPLACE VIEW fishing_effort.v_fishing_effort
-AS WITH base_table(fishing_entity_id, year, sector_type_id, eez_id, gear_id ,effort_gear_id, length_code, m_um, motorisation, kw_boat, number_boats, days_fished, em_factor, sfr, fuel_coeff) AS (
+AS WITH base_table(fishing_entity_id, year, sector_type_id, eez_id, effort_gear_id, length_code, m_um, motorisation, kw_boat, number_boats, days_fished, effort, em_factor, sfr, fuel_coeff) AS (
          SELECT fe.fishing_entity_id,
             fe.year,
             fe.sector_type_id,
             fe.eez_id,
-			fg.gear_id,
-            fe.effort_gear_id,
+            fe.effort_gear_code,
             fe.length_code,
             fe.m_um,
             fe.motorisation,
             fe.kw_boat,
             fe.number_boats,
             fe.days_fished,
+            fe.effort,
                 CASE fe.m_um
                     WHEN 'M'::text THEN
                     CASE fe.sector_type_id
-                        WHEN 3::int THEN 3.0058
-                        WHEN 1::int THEN 3.17
-                        WHEN 2::int THEN 3.0058
-                        WHEN 4::int THEN 3.0058
+                        WHEN 3 THEN 3.0058
+                        WHEN 1 THEN 3.17
+                        WHEN 2 THEN 3.0058
+                        WHEN 4 THEN 3.0058
                         ELSE NULL::numeric
                     END
                     WHEN 'UM'::text THEN
                     CASE fe.sector_type_id
-                        WHEN 3::int THEN 0
-                        WHEN 1::int THEN 0
-                        WHEN 2::int THEN 0
-                        WHEN 4::int THEN 0
+                        WHEN 3 THEN 0
+                        WHEN 1 THEN 0
+                        WHEN 2 THEN 0
+                        WHEN 4 THEN 0
                         ELSE NULL::integer
                     END::numeric
                     ELSE NULL::numeric
@@ -34,18 +34,18 @@ AS WITH base_table(fishing_entity_id, year, sector_type_id, eez_id, gear_id ,eff
                 CASE fe.m_um
                     WHEN 'M'::text THEN
                     CASE fe.sector_type_id
-                        WHEN 3::int THEN 0.00035
-                        WHEN 1::int THEN 0.0002
-                        WHEN 2::int THEN 0.00035
-                        WHEN 4::int THEN 0.00035
+                        WHEN 3 THEN 0.00035
+                        WHEN 1 THEN 0.0002
+                        WHEN 2 THEN 0.00035
+                        WHEN 4 THEN 0.00035
                         ELSE NULL::numeric
                     END
                     WHEN 'UM'::text THEN
                     CASE fe.sector_type_id
-                        WHEN 3::int THEN 0
-                        WHEN 1::int THEN 0
-                        WHEN 2::int THEN 0
-                        WHEN 4::int THEN 0
+                        WHEN 3 THEN 0
+                        WHEN 1 THEN 0
+                        WHEN 2 THEN 0
+                        WHEN 4 THEN 0
                         ELSE NULL::integer
                     END::numeric
                     ELSE NULL::numeric
@@ -53,14 +53,13 @@ AS WITH base_table(fishing_entity_id, year, sector_type_id, eez_id, gear_id ,eff
             fc.fuel_coeff
            FROM fishing_effort.fishing_effort fe
              JOIN fishing_effort.fuel_coeff fc ON fe.year = fc."﻿year"
-             join web.sector_type st on st.sector_type_id = fe.sector_type_id
-			 JOIN fishing_effort.fishing_effort_gear fg ON st.name = fg.sector_type and fe.effort_gear_id = fg.effort_gear_id
+             JOIN sector_type st ON st.sector_type_id = fe.sector_type_id
+             JOIN fishing_effort.fishing_effort_gear fg ON st.name::text = fg.sector_type::text AND fe.effort_gear_code::text = fg.effort_gear_id::text
         )
  SELECT base_table.fishing_entity_id,
     base_table.year,
     base_table.sector_type_id,
     base_table.eez_id,
-	base_table.gear_id,
     base_table.effort_gear_id,
     base_table.length_code,
     base_table.m_um,
@@ -68,11 +67,12 @@ AS WITH base_table(fishing_entity_id, year, sector_type_id, eez_id, gear_id ,eff
     base_table.kw_boat,
     base_table.number_boats,
     base_table.days_fished,
+    base_table.effort,
     base_table.em_factor,
     base_table.sfr,
     base_table.fuel_coeff,
     base_table.kw_boat * base_table.number_boats * base_table.motorisation::double precision * base_table.em_factor::double precision * base_table.sfr::double precision * base_table.fuel_coeff * base_table.days_fished::double precision AS co2
-   FROM base_table
+   FROM base_table;
 
 
 
